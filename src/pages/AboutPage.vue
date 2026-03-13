bc7d82f80b37a174de0d34ed91aeda9638e1ad2f
Mika Ridley <mikaridley26@gmail.com>
2026-03-13 21:29:41 +0200

    mq

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -6,11 +6,23 @@
       <img className="avatar-img" :src="avatarImg" alt="avatar" />
       <img className="hobbies-img" :src="hobbiesImg" alt="about" />
     </div>
-    <img className="about-img" :src="aboutImg" alt="about" />
-    <img className="experience-img" :src="experienceImg" alt="experience" />
+    <picture class="about-img">
+      <source :srcset="aboutImgMobile" media="(max-width: 850px)" />
+      <img :src="aboutImg" alt="about" />
+    </picture>
+    <picture class="experience-img">
+      <source :srcset="experienceImgMobile" media="(max-width: 850px)" />
+      <img :src="experienceImg" alt="experience" />
+    </picture>
     <div class="sec-imgs">
-      <img className="programs-img" :src="programsImg" alt="programs" />
-      <img className="languages-img" :src="languagesImg" alt="programs" />
+      <picture class="programs-img">
+        <source :srcset="programsImgMobile" media="(max-width: 850px)" />
+        <img :src="programsImg" alt="programs" />
+      </picture>
+      <picture class="languages-img">
+        <source :srcset="languagesImgMobile" media="(max-width: 850px)" />
+        <img :src="languagesImg" alt="languages" />
+      </picture>
     </div>
     <div className="resume-container">
       <img className="resume-img" :src="resumeImg" alt="resume" />
@@ -25,39 +37,27 @@
     </div>
   </section>
 </template>
+
 <script setup>
-const avatarImg = new URL("../assets/imgs/about me/avatar.png", import.meta.url)
-  .href;
-const hobbiesImg = new URL(
-  "../assets/imgs/about me/hobbies.png",
-  import.meta.url,
-).href;
-const aboutImg = new URL(
-  "../assets/imgs/about me/about me.png",
-  import.meta.url,
-).href;
-const languagesImg = new URL(
-  "../assets/imgs/about me/languages.png",
-  import.meta.url,
-).href;
-const programsImg = new URL(
-  "../assets/imgs/about me/programs.png",
-  import.meta.url,
-).href;
-const experienceImg = new URL(
-  "../assets/imgs/about me/experience.png",
-  import.meta.url,
-).href;
-const resumeImg = new URL("../assets/imgs/about me/resume.png", import.meta.url)
-  .href;
+import avatarImg from "../assets/imgs/about me/avatar.png";
+import hobbiesImg from "../assets/imgs/about me/hobbies.png";
+import aboutImg from "../assets/imgs/about me/about me.png";
+import aboutImgMobile from "../assets/imgs/about me/about me mobile.png";
+import languagesImg from "../assets/imgs/about me/languages.png";
+import languagesImgMobile from "../assets/imgs/about me/language mobile.png";
+import programsImg from "../assets/imgs/about me/programs.png";
+import programsImgMobile from "../assets/imgs/about me/programs mobile.png";
+import experienceImg from "../assets/imgs/about me/experience.png";
+import experienceImgMobile from "../assets/imgs/about me/experience mobile.png";
+import resumeImg from "../assets/imgs/about me/resume.png";
 </script>
+
 <style scoped>
 .about-page {
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   align-items: end;
   margin-top: 5rem;
-  margin-inline: 8rem;
 
   h2 {
     grid-column: 1 / 7;
@@ -74,23 +74,27 @@ const resumeImg = new URL("../assets/imgs/about me/resume.png", import.meta.url)
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+    margin-block-end: 1rem;
   }
 
   .main-imgs {
     grid-column: 1 / 7;
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(2, max-content);
     align-items: center;
-    gap: 8.6rem;
+    gap: 9rem;
     width: 100%;
+
     .avatar-img {
-      max-width: 35rem;
       width: 100%;
+      height:100%;
+      max-width: 35rem;
     }
 
     .hobbies-img {
-      max-width: 38rem;
       width: 100%;
+      height:100%;
+      max-width: 35rem;
     }
   }
 
@@ -127,21 +131,21 @@ const resumeImg = new URL("../assets/imgs/about me/resume.png", import.meta.url)
   .resume-container {
     grid-column: 1 / 7;
     justify-self: center;
-    display:grid;
+    display: grid;
     justify-items: center;
-    row-gap:1.75rem;
-    padding-block: 8rem;;
+    row-gap: 1.75rem;
+    padding-block: 8rem;
 
-    .resume-img{
-      grid-column:1;
-      grid-row:1/3;
+    .resume-img {
+      grid-column: 1;
+      grid-row: 1/3;
       position: relative;
       z-index: 1;
     }
 
     h2 {
-      grid-column:1;
-      grid-row:1;
+      grid-column: 1;
+      grid-row: 1;
       position: relative;
       z-index: 2;
       align-self: end;
@@ -152,8 +156,8 @@ const resumeImg = new URL("../assets/imgs/about me/resume.png", import.meta.url)
     }
 
     .about-btn {
-      grid-column:1;
-      grid-row:2;
+      grid-column: 1;
+      grid-row: 2;
       position: relative;
       z-index: 2;
       display: grid;
@@ -165,6 +169,99 @@ const resumeImg = new URL("../assets/imgs/about me/resume.png", import.meta.url)
       font-style: normal;
       font-weight: 400;
       line-height: normal;
+    }
+  }
+}
+
+@media (max-width: 850px) {
+  .about-page {
+    grid-template-columns: repeat(2, 1fr);
+    margin-inline: 3rem;
+
+    h2 {
+      grid-column: 1 / 3;
+      font-size: 1.5rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 2rem;
+    }
+
+    h1 {
+      grid-column: 1 / 3;
+      font-size: 3.4375rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+
+    .main-imgs {
+      grid-column: 1 / 3;
+      grid-template-columns: repeat(1, 1fr);
+      gap: 1.3rem;
+      justify-items: center;
+
+      .avatar-img,
+      .hobbies-img {
+        width: 100%;
+        max-width: 100%;
+      }
+    }
+
+    .about-img {
+      grid-column: 1 / 3;
+      place-items: center;
+
+      img{
+        width: 100%;
+      }
+    }
+
+    .experience-img {
+      display: grid;
+      place-items: center;
+
+      img{
+        width: 100%;
+      }
+    }
+
+    .sec-imgs {
+      justify-self: center;
+      grid-column: 1 /3;
+      grid-row: 6;
+      display: grid;
+      grid-template-columns: 1.095fr 1fr;
+      grid-template-rows: repeat(1, 1fr);
+      gap: 0.2rem;
+      margin-block-start: 2.7rem;
+      width:100%;
+
+      img{
+        width: 100%;
+      }
+
+    }
+
+    .resume-container {
+      row-gap: 0.2rem;
+      padding-block:4rem;
+
+      h2 {
+        font-size: 1.125rem;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 2rem;
+      }
+
+      .about-btn {
+        width: 15rem;
+        height: 2.8rem;
+
+        font-size: 1.125rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
     }
   }
 }
